09861e989e1c7a1194a0f385376af5e8f912dff3
Lacayo <glacay30@nca.edu.ni>
2017-02-07 16:39:17 -0600

    Added small error handling; cleaner code

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ import urllib.parse
 import ast
 
 def get_student_credit(username, password):
-    url = 'http://db.nca.edu.ni/api/api_ewapp.php?'
+    api = 'http://db.nca.edu.ni/api/api_ewapp.php?'
     data = {
         'mode': 'student',
         'query': 'login',
         'username': username,
         'password': password,
         }
-    access = url + urllib.parse.urlencode(data)
-    # access is the actual url that is accessed
+    url = api + urllib.parse.urlencode(data)
+    # creates the url with the 
     
-    with urllib.request.urlopen(access) as response:
-        student_credit = ast.literal_eval(response.read().decode())['credit_student']
-        # the above reads the page, decodes it (byte to str), evals it as a dictionary, then 
-        # gets the value of 'credit_student' and stores it into the variable student_credit
-        print(student_credit)
-        # student credit is the amount of money in the account
+    with urllib.request.urlopen(url) as response:
+        page_raw = response.read()
+        page_str = page_raw.decode()
+        page_dict = ast.literal_eval(page_str)
+        credit_student_amount = page_dict['credit_student']
+        
+        if page_dict['login'] == 0:
+            print("Error with login, please try again.")
+        elif page_dict['login'] == 1:
+            print(credit_student_amount)
 
 get_student_credit(input("Username: "), input("Password: "))
 # main function that takes the NCA username and password everyone has
