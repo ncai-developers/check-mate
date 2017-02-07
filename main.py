b0882069f767355c78e586503813d9bc2921cd75
Lacayo <glacay30@nca.edu.ni>
2017-02-07 16:24:27 -0600

    Improved Documentation
    added more comments for clarification

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ import ast
 
 def get_student_credit(username, password):
     url = 'http://db.nca.edu.ni/api/api_ewapp.php?'
-    data = {'mode': 'student',
-              'query': 'login',
-              'username': username,
-              'password': password}
+    data = {
+        'mode': 'student',
+        'query': 'login',
+        'username': username,
+        'password': password,
+        }
     access = url + urllib.parse.urlencode(data)
+    # access is the actual url that is accessed
     
     with urllib.request.urlopen(access) as response:
-       student_credit = ast.literal_eval(response.read().decode())['credit_student']
-       # the above reads the page, decodes it (byte to str), evals it as a dictionary, then 
-       # gets the value of 'credit_student' and stores it into the variable student_credit
-       print(student_credit)
+        student_credit = ast.literal_eval(response.read().decode())['credit_student']
+        # the above reads the page, decodes it (byte to str), evals it as a dictionary, then 
+        # gets the value of 'credit_student' and stores it into the variable student_credit
+        print(student_credit)
+        # student credit is the amount of money in the account
 
-get_student_credit(input(), input())
+get_student_credit(input("Username: "), input("Password: "))
+# main function that takes the NCA username and password everyone has
