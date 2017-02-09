d3b349f5fa900fb04272b4f3b71b8b70e1d42224
Lacayo <glacay30@nca.edu.ni>
2017-02-09 15:58:50 -0600

    Fixed Error Handling + Added getpass
    Any invalid login information will now output "Error with login, please try again." It does this by seeing whether there is null, and if it does, then it will know that something went wrong. Also added getpass, which hides the user input; in this case, hiding the password typed in.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import urllib.request
 import urllib.parse
 import ast
+import getpass
+
 
 def get_student_credit(username, password):
+    # function that takes the NCA username and password, and outputs the credit student balance
+    
     api = 'http://db.nca.edu.ni/api/api_ewapp.php?'
     data = {
         'mode': 'student',
@@ -11,18 +15,20 @@ def get_student_credit(username, password):
         'password': password,
         }
     url = api + urllib.parse.urlencode(data)
-    # creates the url with the 
+    # data must be encoded for url to function properly
     
     with urllib.request.urlopen(url) as response:
         page_raw = response.read()
         page_str = page_raw.decode()
-        page_dict = ast.literal_eval(page_str)
-        credit_student_amount = page_dict['credit_student']
         
-        if page_dict['login'] == 0:
+        if 'null' in page_str:
             print("Error with login, please try again.")
-        elif page_dict['login'] == 1:
-            print(credit_student_amount)
+            # python doesn't know what null means, so it cannot be evaluated
+            # any typos will create null, thus we know there is an error
+        else:
+            page_dict = ast.literal_eval(page_str)
+            credit_student_balance = page_dict['credit_student']
+            print(credit_student_balance)
 
-get_student_credit(input("Username: "), input("Password: "))
-# main function that takes the NCA username and password everyone has
+get_student_credit(input("Username: "), getpass.getpass())
+# getpass is exactly like input() but does not echo the typed characters, hiding it
