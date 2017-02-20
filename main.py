7c1ff54a4efa117848fcd548d10e26baab3d0f02
Lacayo <glacay30@nca.edu.ni>
2017-02-20 15:20:13 -0600

    Getpass more explicit + print more informative

--- main.py
+++ main.py
@@ -26,16 +26,16 @@ def get_student_credit(username, password):
             print("Error with login, please try again.")
             # python doesn't know what null means, so it cannot be evaluated
             # any typos will create null, thus raising an exception, so we'll
-            # end the function in this if statement
+            # end the function by looking for null in this if statement
         else:
             page_dict = ast.literal_eval(page_str)
             credit_student_balance = page_dict['credit_student']
-            print(credit_student_balance)
+            print("Student Credit: ${}".format(credit_student_balance))
 
 while True:
     try:
-        get_student_credit(input("Username: "), getpass.getpass())
-        # getpass is the same as input, but you won't see what you type
+        get_student_credit(input("Username: "), getpass.getpass("Password: "))
+        # getpass is the same as input("Password: "), but you won't see what you type
     except KeyboardInterrupt:
         sys.exit(0)
         # Control-C will raise a KeyboardInterrupt exception, our method of quitting
