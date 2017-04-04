ca64878fd5525d3a78fd9511694dfba6fed13eeb
Hellsing76 <Hellsing76@users.noreply.github.com>
2017-04-03 23:07:54 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,14 +1,8 @@
 
 import requests
 
-# Important Note:
-# the functions here serve to use different APIs
-# to change which function is used, navigate to main.py and change the function there
-# I (Lacayo) will be using login instead of mock for my GUI until mock is up and running
-
 
 def get_credit_with_mock(user_id):
-    # function using NCAI login to get credit_student and credit_family
     try:
         api = "http://localhost:3000/api/"
         response = requests.get(api + user_id).json()
@@ -21,5 +15,17 @@ def get_credit_with_mock(user_id):
         return "Please enter an id"
     except KeyboardInterrupt:
         return "Goodbye"
-  
-        
+
+def get_credit_in_cords_with_mock(user_id):
+    try:
+        api = "http://localhost:3000/api/"
+        response = requests.get(api + user_id).json()
+        return "Balance: C$ {}\nFamily balance: C${}".format(response['credit_student']*29.5, response['credit_family']*29.5)
+    except requests.exceptions.ConnectionError:
+        return "Connection unable to be established. Is the server online?"
+    except KeyError:
+        return "Invalid id. Please try again."
+    except TypeError:
+        return "Please enter an id"
+    except KeyboardInterrupt:
+        return "Goodbye"
